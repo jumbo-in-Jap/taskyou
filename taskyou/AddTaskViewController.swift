87cae9d7a6c0630a84297cc500f744e104bbd7eb
jumbo ken <subsubmailer@gmail.com>
2014-08-03 17:44:36 +0900

    add delete methoe

---
+++ taskyou/AddTaskViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddTaskViewController.swift
+//  taskyou
+//
+//  Created by 羽田 健太郎 on 2014/08/03.
+//  Copyright (c) 2014年 me.jumbeeee.ken. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
