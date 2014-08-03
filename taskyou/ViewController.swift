0ec9ddf64ab602ef52bea85e649d36fd72391d3a
jumbo ken <subsubmailer@gmail.com>
2014-08-03 13:35:53 +0900

    Initial Commit

---
+++ taskyou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taskyou
+//
+//  Created by 羽田 健太郎 on 2014/08/03.
+//  Copyright (c) 2014年 me.jumbeeee.ken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
